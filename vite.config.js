027263bd5179e3b583bd9f2da2b5d0bb3caa2b84
RohiniShankari <136949412+RohiniShankari@users.noreply.github.com>
2025-07-09 11:41:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Required for Render to access the app
+    port: 5173,       // Optional, but keeps it predictable
+    allowedHosts: ['goals-frontend-7wob.onrender.com'] // Allow Render's domain
+  }
 })
+
